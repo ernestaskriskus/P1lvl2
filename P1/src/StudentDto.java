06759cbe460900f0e1559b8fbb782be34ae90f2f
Ernestas <ernestas.kriskus@gmail.com>
2023-10-16 12:20:40 -0500

    StudentDto.java fiel surName added

--- P1/src/StudentDto.java
+++ P1/src/StudentDto.java
@@ -1,11 +1,17 @@
 public class StudentDto {
     private final String name;
+    private final String surName;
 
-    public StudentDto(String name) {
+    public StudentDto(String name, String surName) {
         this.name = name;
+        this.surName = surName;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getSurName() {
+        return surName;
     }
 }
