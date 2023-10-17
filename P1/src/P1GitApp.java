7e1bb6b1148dc9eaa5916eb4449607a62b4a43a3
Ernestas <ernestas.kriskus@gmail.com>
2023-10-17 11:46:33 -0500

    [if git] merge conflict

--- P1/src/P1GitApp.java
+++ P1/src/P1GitApp.java
@@ -1,17 +1,24 @@
 public class P1GitApp {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        ifExample();
     }
 
-    ifExample();
-
-    private static void ifExample() {
-        int result = 0;
-        if (5 > 3) {
-            result = true;
+    private static void ifExample(){
+        int dayValue = 6;
+        if (dayValue >= 1 && dayValue <= 5){
+            working();
+        } else if (dayValue >= 6 && dayValue <= 7) {
+            resting();
         } else {
-            result = false;
+            System.err.println("day value is wrong!");
         }
     }
+private static void working(){
+    System.out.println("I am working");
+}
+private static void resting(){
+    System.out.println("Resting");
+}
 
 }
